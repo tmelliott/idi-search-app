85b2a09f987918d63590c4342e88184059e0a238
Tom Elliott <tomelliottnz@gmail.com>
2021-11-02 17:30:50 +1300

    fix api async/await

--- pages/api/collections/index.js
+++ pages/api/collections/index.js
@@ -2,6 +2,6 @@ import getCollections from "../../../components/database/collections"
 
 export default async function collectionsAPI(req, res) {
   const query = req.query.q
-  const collections = getCollections(query)
+  const collections = await getCollections(query)
   res.status(200).json(collections)
 }
--- pages/api/datasets/index.js
+++ pages/api/datasets/index.js
@@ -1,6 +1,6 @@
 import getDatasets from "../../../components/database/datasets"
 export default async function datasetsAPI(req, res) {
   const query = req.query.q
-  const datasets = getDatasets(query)
-  datasets
+  const datasets = await getDatasets(query)
+  res.status(200).json(datasets)
 }
