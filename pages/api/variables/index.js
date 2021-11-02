33369b5ca2db685ce0ec0084246ef6d686caf01c
Tom Elliott <tomelliottnz@gmail.com>
2021-11-02 17:24:40 +1300

    add parameter

--- pages/api/variables/index.js
+++ pages/api/variables/index.js
@@ -2,6 +2,6 @@ import getVariables from "../../../components/database/variables"
 
 export default async function variablesAPI(req, res) {
   const query = req.query.q
-  const variables = await getVariables()
+  const variables = await getVariables(query)
   res.status(200).json(variables)
 }
