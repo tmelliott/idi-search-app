76b84e2d31b85e6debd0e5e5be92da2189b25897
Tom Elliott <tomelliottnz@gmail.com>
2022-10-18 12:27:26 +1300

    update help/about

--- components/layout/ProseLayout.js
+++ components/layout/ProseLayout.js
@@ -1,12 +1,44 @@
+import { renderToString } from "react-dom/server"
 import Head from "next/head"
 
+const getHeadings = (source) => {
+  const regex = /<h2>(.*?)<\/h2>/g
+  if (source.match(regex)) {
+    return source.match(regex).map((heading) => {
+      const text = heading.replace("<h2>", "").replace("</h2>", "")
+      const link = "#" + text.replace(/ /g, "_").toLowerCase()
+      return { text, link }
+    })
+  }
+  return []
+}
+
 const ProseLayout = ({ meta, children }) => {
+  const contentString = renderToString(children)
+  const headings = getHeadings(contentString)
+
   return (
     <>
       <Head>
         <title>{meta?.title || "IDI Search App"}</title>
       </Head>
-      <div className="about prose prose-sm prose-blue mx-auto">{children}</div>
+
+      <div className="about mx-auto py-12 flex flex-col max-w-3xl lg:max-w-none lg:flex-row gap-8 justify-center lg:h-full">
+        <div className="flex flex-col prose prose-sm prose-blue px-4">
+          <h4>Table of Contents</h4>
+          <ul className="list-none">
+            {headings.map((heading) => (
+              <li key={heading.text}>
+                <a href={heading.link}>{heading.text}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="prose  prose-blue max-w-3xl lg:overflow-x-scroll">
+          {children}
+        </div>
+      </div>
     </>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,16 @@ import { useState } from "react"
 
 import MainLayout from "../components/layout/Layout"
 import PlausibleProvider from "next-plausible"
+import { MDXProvider } from "@mdx-js/react"
+
+const HeaderH2 = ({ children }) => {
+  const idText = children.replace(/ /g, "_").toLowerCase()
+  return <h2 id={idText}>{children}</h2>
+}
+
+const components = {
+  h2: HeaderH2,
+}
 
 function MyApp({ Component, pageProps }) {
   const [filterTerm, setFilterTerm] = useState("")
@@ -17,13 +27,15 @@ function MyApp({ Component, pageProps }) {
       selfHosted="true"
       enabled="true"
     >
-      <Layout>
-        <Component
-          {...pageProps}
-          filterTerm={filterTerm}
-          setFilterTerm={setFilterTerm}
-        />
-      </Layout>
+      <MDXProvider components={components}>
+        <Layout>
+          <Component
+            {...pageProps}
+            filterTerm={filterTerm}
+            setFilterTerm={setFilterTerm}
+          />
+        </Layout>
+      </MDXProvider>
     </PlausibleProvider>
   )
 }
