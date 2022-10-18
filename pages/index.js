db230fa018fd59acc08601deaa534c71919c152c
Tom Elliott <tomelliottnz@gmail.com>
2022-10-19 10:11:00 +1300

    add metadata to index

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,44 @@ function Home() {
   return (
     <>
       <Head>
+        {/* <!-- Primary Meta Tags --> */}
         <title>IDI Search | What's in the IDI?</title>
+        <meta name="title" content="IDI Search | What's in the IDI?" />
         <meta
           name="description"
-          content="A tool for exploring the data available in New Zealand's IDI"
+          content="A tool for exploring the data available in New Zealand's IDI."
+        />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://idisearch.terourou.org/" />
+        <meta property="og:title" content="IDI Search | What's in the IDI?" />
+        <meta
+          property="og:description"
+          content="A tool for exploring the data available in New Zealand's IDI."
+        />
+        <meta
+          property="og:image"
+          content="https://idisearch.terourou.org/logo.png"
+        />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://idisearch.terourou.org/"
+        />
+        <meta
+          property="twitter:title"
+          content="IDI Search | What's in the IDI?"
+        />
+        <meta
+          property="twitter:description"
+          content="A tool for exploring the data available in New Zealand's IDI."
+        />
+        <meta
+          property="twitter:image"
+          content="https://idisearch.terourou.org/logo.png"
         />
       </Head>
 
