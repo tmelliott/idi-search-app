a72e30727bf4cf29212707095a708cfe526e4c82
Tom Elliott <tomelliottnz@gmail.com>
2022-11-20 09:30:48 +1300

    add a basic write/delete endpoint to keep db alive

---
+++ pages/api/admin/prevent_sleep.js
@@ -0,0 +1,22 @@
+import { prisma } from "../../../lib/db"
+
+// This API end point can be called to trigger a write-and-read of the database,
+// preventing it from going to sleep!
+export default async function preventSleep(req, res) {
+  res.setHeader("Cache-Control", "s-maxage=604800")
+  const query = req.query.q
+  const newAgency = await prisma.agencies.create({
+    data: {
+      agency_id: "test",
+      agency_name: "Test Agency",
+    },
+  })
+  console.log("Created: ", newAgency)
+  const dropAgency = await prisma.agencies.delete({
+    where: {
+      agency_id: "test",
+    },
+  })
+  console.log("Deleted: ", dropAgency)
+  res.status(200).json("OK")
+}
