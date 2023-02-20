3a483e97880fb27fe5452bf4cc3ec3bb27b53cc7
Tom Elliott <tomelliottnz@gmail.com>
2023-02-21 11:58:30 +1300

    delete console.logs

--- components/Dataset.js
+++ components/Dataset.js
@@ -19,8 +19,6 @@ function Dataset({ id, term }) {
   if (isLoading) return <CogIcon className="h-10 animate-spin-slow mb-4" />
   if (error) return <>Error ...</>
 
-  console.log(dataset)
-
   let description = dataset.description || ""
   if (highlight) {
     const searchMask = `(${highlight.replaceAll(" ", "|")})`
