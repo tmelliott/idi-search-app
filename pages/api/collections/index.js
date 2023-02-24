6e054567ed9b1ab8c51ce0eac298c299842030fb
Tom Elliott <tomelliottnz@gmail.com>
2023-02-24 18:50:06 +1300

    init new t3 nextjs app

--- pages/api/collections/index.js
+++
@@ -1,8 +0,0 @@
-import getCollections from "../../../components/database/collections"
-
-export default async function collectionsAPI(req, res) {
-  res.setHeader("Cache-Control", "s-maxage=604800")
-  const { q, agencyId } = req.query
-  const collections = await getCollections(q, agencyId)
-  res.status(200).json(collections)
-}
