8e86de5a7267f4a2f31d6b064d5d56dd94304aa9
Tom Elliott <tomelliottnz@gmail.com>
2023-03-14 15:31:52 +1300

    (app): SSG of index and agencies pages

--- src/pages/agencies/index.tsx
+++ src/pages/agencies/index.tsx
@@ -1,4 +1,10 @@
+import { type GetStaticProps } from "next";
+
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import superjson from "superjson";
 import HeadTags from "~/layout/Head";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
 import { type NextPageWithLayout } from "~/types/types";
 
 import Agencies from "../../components/Agencies";
@@ -16,3 +22,20 @@ const AgenciesPage: NextPageWithLayout = () => {
 
 AgenciesPage.Layout = "Dual";
 export default AgenciesPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: { prisma },
+    transformer: superjson,
+  });
+
+  await ssg.agencies.all.fetch({});
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+    },
+    revalidate: 1,
+  };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,55 @@
+import { type GetStaticProps } from "next";
+
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import superjson from "superjson";
 import Agencies from "~/components/Agencies";
 import Collections from "~/components/Collections";
 import Datasets from "~/components/Datasets";
 import Variables from "~/components/Variables";
 import HeadTags from "~/layout/Head";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
 import { type NextPageWithLayout } from "~/types/types";
+
+const LIMITS = {
+  agencies: 2,
+  collections: 3,
+  datasets: 5,
+  variables: 10,
+};
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
       <HeadTags />
 
-      <Agencies limit={2} />
-      <Collections limit={3} />
-      <Datasets limit={5} />
-      <Variables limit={10} />
+      <Agencies limit={LIMITS.agencies} />
+      <Collections limit={LIMITS.collections} />
+      <Datasets limit={LIMITS.datasets} />
+      <Variables limit={LIMITS.variables} />
     </>
   );
 };
 
 Home.Layout = "Dual";
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: { prisma },
+    transformer: superjson,
+  });
+
+  await ssg.agencies.all.fetch({ limit: LIMITS.agencies });
+  await ssg.collections.all.fetch({ limit: LIMITS.collections });
+  await ssg.datasets.all.fetch({ limit: LIMITS.datasets });
+  await ssg.variables.all.fetch({ limit: LIMITS.variables });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+    },
+    revalidate: 1,
+  };
+};
