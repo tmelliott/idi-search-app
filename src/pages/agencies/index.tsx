509eaacef081464aaada6c025fad6a90986beb54
Tom Elliott <tomelliottnz@gmail.com>
2023-03-14 16:08:05 +1300

    undo ssr stuff - needs more thought

--- src/pages/agencies/index.tsx
+++ src/pages/agencies/index.tsx
@@ -1,10 +1,4 @@
-import { type GetStaticProps } from "next";
-
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
-import superjson from "superjson";
 import HeadTags from "~/layout/Head";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
 import { type NextPageWithLayout } from "~/types/types";
 
 import Agencies from "../../components/Agencies";
@@ -23,19 +17,19 @@ const AgenciesPage: NextPageWithLayout = () => {
 AgenciesPage.Layout = "Dual";
 export default AgenciesPage;
 
-export const getStaticProps: GetStaticProps = async () => {
-  const ssg = createProxySSGHelpers({
-    router: appRouter,
-    ctx: { prisma },
-    transformer: superjson,
-  });
+// export const getStaticProps: GetStaticProps = async () => {
+//   const ssg = createProxySSGHelpers({
+//     router: appRouter,
+//     ctx: { prisma },
+//     transformer: superjson,
+//   });
 
-  await ssg.agencies.all.fetch({});
+//   await ssg.agencies.all.fetch({});
 
-  return {
-    props: {
-      trpcState: ssg.dehydrate(),
-    },
-    revalidate: 1,
-  };
-};
+//   return {
+//     props: {
+//       trpcState: ssg.dehydrate(),
+//     },
+//     revalidate: 1,
+//   };
+// };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
-import { type GetStaticProps } from "next";
-
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
-import superjson from "superjson";
 import Agencies from "~/components/Agencies";
 import Collections from "~/components/Collections";
 import Datasets from "~/components/Datasets";
 import Variables from "~/components/Variables";
 import HeadTags from "~/layout/Head";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
 import { type NextPageWithLayout } from "~/types/types";
 
 const LIMITS = {
@@ -34,22 +28,22 @@ const Home: NextPageWithLayout = () => {
 Home.Layout = "Dual";
 export default Home;
 
-export const getStaticProps: GetStaticProps = async () => {
-  const ssg = createProxySSGHelpers({
-    router: appRouter,
-    ctx: { prisma },
-    transformer: superjson,
-  });
+// export const getStaticProps: GetStaticProps = async () => {
+//   const ssg = createProxySSGHelpers({
+//     router: appRouter,
+//     ctx: { prisma },
+//     transformer: superjson,
+//   });
 
-  await ssg.agencies.all.fetch({ limit: LIMITS.agencies });
-  await ssg.collections.all.fetch({ limit: LIMITS.collections });
-  await ssg.datasets.all.fetch({ limit: LIMITS.datasets });
-  await ssg.variables.all.fetch({ limit: LIMITS.variables });
+//   await ssg.agencies.all.fetch({ limit: LIMITS.agencies });
+//   await ssg.collections.all.fetch({ limit: LIMITS.collections });
+//   await ssg.datasets.all.fetch({ limit: LIMITS.datasets });
+//   await ssg.variables.all.fetch({ limit: LIMITS.variables });
 
-  return {
-    props: {
-      trpcState: ssg.dehydrate(),
-    },
-    revalidate: 1,
-  };
-};
+//   return {
+//     props: {
+//       trpcState: ssg.dehydrate(),
+//     },
+//     revalidate: 1,
+//   };
+// };
