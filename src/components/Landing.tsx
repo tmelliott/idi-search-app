9b2075cbc81ce50568fc29a77124fbe7a04379cd
Tom Elliott <tomelliottnz@gmail.com>
2023-06-20 12:20:41 +1200

    more logging

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,6 +6,7 @@ import { api } from "~/utils/api";
 const LandingPage = () => {
   const { data: info } = api.db_info.get.useQuery();
   console.log(process.env.NEXT_PUBLIC_UPDATED_AT);
+  console.log(dayjs(process.env.NEXT_PUBLIC_UPDATED_AT));
   const stats = info
     ? [
         {
